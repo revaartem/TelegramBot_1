7ab11e7fc9c08865426d21e6a1a94891b35ff271
Reva <revaartem2008@gmail.com>
2022-12-01 20:33:20 +0200

    Started work

--- TelegramBot.py
+++ TelegramBot.py
@@ -10,14 +10,17 @@ import os
 bot = telebot.TeleBot(config.token)
 
 
-# @bot.message_handler(commands=['start'])
-# def message_start(message):
-#     bot.send_message(message.chat.id, 'Hello, user!')
-
-
 @bot.message_handler(commands=['start'])
 def message_start(message):
+    bot.send_message(message.chat.id, 'Вітаю. Я бот єУкриття. Якщо ти в небезпеці, хутчіш тицяй на команду списку '
+                                      'укриттів та вибирай найближче!')
+
+
+@bot.message_handler(commands=['shelters'])
+def message_start(message):
     keyboard = telebot.types.InlineKeyboardMarkup(row_width=2)
+
+
 
     with open('shelters.txt') as file:
         shelters = [item.split(',') for item in file]
@@ -28,15 +31,38 @@ def message_start(message):
             url_button = telebot.types.InlineKeyboardButton(text=source[0].strip(), url=source[1].strip())
             keyboard.add(url_button)
 
-        bot.send_message(message.chat.id, 'Список укрытий', reply_markup=keyboard)
+        bot.send_message(message.chat.id, 'Список укриттів', reply_markup=keyboard)
 
 
 @bot.message_handler(content_types=['text'])
 def message_text(message):
-    if message.lower().startswith('слава україні'):
+    if message.text.lower().startswith('слава нації'):
+        bot.send_message(message.chat.id, 'Смерть ворогам!')
+
+    elif message.text.lower().startswith('слава україні'):
         bot.send_message(message.chat.id, 'Героям Слава!')
-    elif message.lower().startswith('слава нації'):
-        bot.send_message(message.chat.id, 'Смерть ворогам! Україна понад усе!')
+
+    elif message.text.lower().startswith('слава'):
+        bot.send_message(message.chat.id, 'Україні!')
+
+    elif message.text.lower().startswith('героям'):
+        bot.send_message(message.chat.id, 'Слава!')
+
+    elif message.text.lower().startswith('україна'):
+        bot.send_message(message.chat.id, 'Понад усе!')
+
+    elif message.text.lower().startswith('руский') or\
+         message.text.lower().startswith('русский') or\
+         message.text.lower().startswith('рускій') or\
+         message.text.lower().startswith('русскій'):
+        bot.send_message(message.chat.id, 'Иди нахуй!')
+
+    elif message.text.lower().startswith('путин') or\
+         message.text.lower().startswith('путін'):
+        bot.send_message(message.chat.id, 'Хуйло!')
+    else:
+        bot.send_message(message.chat.id, 'Пробач, не розумію тебе. Спробуй ще раз.')
+
 
 
 bot.polling(none_stop=True)
--- temp.py
+++ temp.py
@@ -3,3 +3,7 @@
 # ['Одесская область', https://www.google.com/maps/d/u/0/viewer?mid=1f7Nswyb-hXG0wkFDhyGYFbrANok&hl=ru&fbclid=IwAR3WQENPPGYdz60vu2FC6JHx0JhEI9DRrH_eVYGv8K_rhnIKFAHdBRUHWp4&ll=46.84347251824935%2C29.81425686856717&z=8]
 # ['Сумы', https://www.google.com/maps/d/u/0/viewer?mid=1U6JH6sTiCW37oU3nITeUTx5C5-k&ll=50.89910517378453%2C34.86530394497134&z=12]
 # ['Полтавская область', https://www.google.com/maps/d/u/0/viewer?mid=10wBVAAKCTHdPXYODiUbhjTTrJoY&ll=49.834675436347666%2C34.590847172723&z=8]
+
+
+x = 'слава'
+print(x.startswith('слава'))
